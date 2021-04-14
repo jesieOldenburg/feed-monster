04e6e0fe2fe711d8c50f95620956075b0d93d829
Jesie Oldenburg <kitglo46@gmail.com>
2021-04-13 22:58:12 -0500

    Initial push to repo.  Basic project setup

---
+++ menu.py
@@ -0,0 +1,32 @@
+import os
+
+def create_main_menu():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    # print('+' + '-++' *16 + '-+')
+    # print('|  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |' )
+    # print('+' + '-++' *16 + '-+\n')
+    # print(f'+{'-++' * 16}-+')
+    # TODO: Logic for searching for URL Feeds
+    print("1. Display Subscribed Feeds") 
+
+    # TODO: Write logic for displaying feeds for option 1.
+        # * Need a function to build the menu for this option. This menu should display an option to input a URL, as well as a prompt
+    print("2. Input New RSS URL")
+    # print("3. Feed Animal")
+    # print("4. Cultivate New Plant")
+    # print("5. Show Arboretum Report")
+    print("3. Exit\n")
+    print("CHOOSE AN OPTION")
+    pass
+
+def main_menu_logic():
+    create_main_menu()
+    menu_selection = input(">> ")
+    # TODO: Write some if statements to determine the function to be called based off of the menu choice.
+    # TODO: Write the functions that handle the menu choices in a separate module.
+    if menu_selection == 1: pass
+    if menu_selection == 2: pass
+    pass
+
+def display_feeds_table():
+    pass
---
+++ monster.py
@@ -0,0 +1,10 @@
+import urllib.request
+import xml.dom.minidom
+
+
+with urllib.request.urlopen('http://news.yahoo.com/rss/') as response:
+    xml_response = response.read()
+
+dom = xml.dom.minidom.parseString(xml_response)
+pretty_xml = dom.toprettyxml()
+print(pretty_xml)
