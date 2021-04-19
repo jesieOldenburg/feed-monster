d684ba0bba1b40c0f8da6a47c0d106983d59db47
Jesie Oldenburg <kitglo46@gmail.com>
2021-04-19 02:00:17 -0500

    working on persisting data with shelve

--- menu.py
+++ menu.py
@@ -41,14 +41,12 @@ def main_menu_logic():
 
 def display_feeds_table():
     # TODO Need to try and format my own table. Draw it out first
-    # print('|' + '--' * 76 + '| \n')
-    # print('|' + '--' * 76 + '|')
     PT.field_names = ['Title', 'Description', 'Link']
     # print(PT)
     pass
 
-def create_table_rows(ti, de, li):
-    PT.add_row([ti, de, li])
+def create_table_rows(title, desc, link):
+    PT.add_row([title, desc, link])
     
 
 def display_rss_url_search_menu():
@@ -58,7 +56,7 @@ def display_rss_url_search_menu():
     get_rss_url(feed_to_search) # change this back to this: get_rss_url(RSS_URL_input)
     pass
 
-feed_to_search = 'http://www.cbn.com/cbnnews/us/feed/'
+feed_to_search = 'http://feeds.bbci.co.uk/news/world/rss.xml'
 # ? Test Feed URL: 'http://feeds.bbci.co.uk/news/world/rss.xml'
 # ? Test Feed URL: 'http://www.cbn.com/cbnnews/us/feed/'
 # ? Test Feed URL: 'https://thewest.com.au/rss-feeds'
@@ -70,11 +68,36 @@ feed_to_search = 'http://www.cbn.com/cbnnews/us/feed/'
 
 """
 
+def store_data(d):
+    # Needs to use shelve to persist the data
+    SH = shelve.open('feeds.db')
+    try:
+        SH['feeds'] = d
+        SUBBED_FEEDS = SH['feeds']
+    finally:
+        SH.close()
+    print('SUB FEED', SUBBED_FEEDS)
+
+master_dict = {
+    'feeds': {}
+}
+
 def parse_XML(xml_str):
     # TODO: typecheck the root tag to be <rss>
     root = ET.fromstring(xml_str) # grabs the root tag from the XML response [str], which will always be an RSS tag
     title_tag_list = root.findall('.//item') # Finds all tags named item in the XML response
+    feed_title = root[0][0].text
+    feed_link = root[0][1].text
+    print('feed title', root[0][1].text)
     display_feeds_table()
+    
+    if feed_title in master_dict['feeds'].keys():
+        print('You are already subscribed to this feed')
+    else: 
+        master_dict['feeds'][feed_title] = feed_link
+        store_data(master_dict)
+        pass
+    print(master_dict)
     # TODO These values will need to be stored in a separate data structure.  The values will need to be pulled for displaying in the feeds table when the user inits the menu. Consider creating a function that fetches a few URLs once the program is ran, and loads those into the table.
     for item_tag in title_tag_list[1:11]: # limit the results to ten 
         article_title = item_tag[0].text[:10]
@@ -82,8 +105,8 @@ def parse_XML(xml_str):
         article_description = item_tag[1].text[:10]
         # print('DESC ==>', article_description)
         article_link = item_tag[3].tag # Change me back to link # TODO: Figure out how to put the url into a different string
-        print('LINK ==>', item_tag[3])
-        create_table_rows(article_title, article_description, article_link)
+        # print('LINK ==>', ite)
+        create_table_rows(article_title, article_description, article_link) # ! This will need to move. TEST ONLY!
         pass
     print(PT)
     # display_feeds_table(article_title, article_description, article_link)
@@ -106,7 +129,6 @@ def get_rss_url(feed_url):
     dom = xml.dom.minidom.parseString(xml_response) # Parses the response to a string
     xml_response = dom.toprettyxml() # Prettify the response
     parse_XML(xml_response)
-
     # user_passed_URL = feed_url
     # tree = ET.parse(pretty_xml)
     # print(tree)
